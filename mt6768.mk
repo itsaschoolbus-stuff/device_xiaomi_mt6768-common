495437aaa65db9f65bab830493565e8bb2fdcae4
itsaschoolbus <itsaschoolbus@proton.me>
2026-07-31 18:14:19 +0700

    revert "mt6768-common: Inherit telephony-common-stub"
    This reverts commit 7842592a506ee82ceea22aacafd139117139498e.

--- mt6768.mk
+++ mt6768.mk
@@ -106,7 +106,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek-recovery
 
 # IMS
-$(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
 $(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_COPY_FILES += \
