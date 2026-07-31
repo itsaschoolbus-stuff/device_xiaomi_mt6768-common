2ec43cd99da07af9971fcd04265a9e5b5f2bb9b3
itsaschoolbus <itsaschoolbus@proton.me>
2026-07-31 11:49:39 +0700

    mt6768-common: patch libcodec2_vndk-v31.so to depend on libui-v30.so
    * error: Unresolved symbol: _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_
    * error: Unresolved symbol: _ZN7android19GraphicBufferMapper6unlockEPK13native_handle

--- extract-files.py
+++ extract-files.py
@@ -88,6 +88,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so')
         .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v31.so'),
     'vendor/lib64/libcodec2_vndk-v31.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v30.so')
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'vendor/lib64/libsfplugin_ccodec_utils-v31.so': blob_fixup()
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so'),
