50984fae92cee67229c39e72a8f18a86fbd7c639
itsaschoolbus <itsaschoolbus@proton.me>
2026-07-31 20:34:46 +0700

    mt6768-common: include ViPER4Android FX

--- mt6768.mk
+++ mt6768.mk
@@ -253,6 +253,10 @@ $(call soong_config_set_bool,mediatek_gadget,use_custom_usb_gadget_rc,true)
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
+# ViPER4Android FX
+TARGET_INCLUDE_VIPERFX := true
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Wi-Fi
 $(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
 
